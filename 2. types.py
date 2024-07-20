f36a8067a9f691504f2e1be0139143f32361aacc
Sergey Evseev <jaimore.evseev@gmail.com>
2024-07-20 21:30:23 +0400

    2. Type system basics.

---
+++ 2. types.py
@@ -0,0 +1,27 @@
+i = 5
+print(i)
+print(type(i))
+
+i = 5.5
+print(i)
+print(type(i))
+
+i = "text"
+print(i)
+print(type(i))
+
+i = []
+print(i)
+print(type(i))
+
+i = {}
+print(i)
+print(type(i))
+
+i = ()
+print(i)
+print(type(i))
+
+i = object
+print(i)
+print(type(i))
